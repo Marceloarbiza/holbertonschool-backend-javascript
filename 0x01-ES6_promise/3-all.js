fd9fefe3446d9a73074bfd53909123c1f3e1e0e1
root <3338@holbertonschool.com>
2022-04-12 21:17:48 +0000

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,7 @@
-export default function handleProfileSignup() {
+import { uploadPhoto, createUser } from './utils';
 
+export default function handleProfileSignup() {
+  const up = uploadPhoto();
+  const cr = createUser();
+  Promise.all([up, cr]).then((values) => { console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`); });
 }
