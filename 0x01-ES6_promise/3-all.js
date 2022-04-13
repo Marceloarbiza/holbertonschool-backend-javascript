e2acb6cb048123526eee30e0d8da485c1b246e12
root <3338@holbertonschool.com>
2022-04-13 23:28:26 +0000

    D

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const up = uploadPhoto();
   const cr = createUser();
-  Promise.all([up, cr]).then((values) => { console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`); }).catch(() => { console.log('Signup system offline'); });
+  Promise.all([up, cr]).then((values) => { console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`); });
 }
