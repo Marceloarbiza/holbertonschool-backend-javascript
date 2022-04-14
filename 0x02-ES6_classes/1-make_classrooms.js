287a3d3aace0e1d4fd48a6967f863afd44ed61cb
root <3338@holbertonschool.com>
2022-04-14 13:52:49 +0000

    classes1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const obj19 = new ClassRoom(19);
+  const obj20 = new ClassRoom(20);
+  const obj34 = new ClassRoom(34);
+  return [obj19, obj20, obj34];
+}
---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    }
+    if (students.isArray !== 'undefined') {
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set length(length) {
+    this._length= length;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+}
