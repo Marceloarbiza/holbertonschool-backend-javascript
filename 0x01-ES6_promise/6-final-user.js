b3b58c0811b98e5a295ae16f49d45d3152dbd274
root <3338@holbertonschool.com>
2022-04-14 01:11:07 +0000

    D

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,24 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const si = signUpUser(firstName, lastName);
-  const up = uploadPhoto(fileName);
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
 
-  return Promise.all([si, up]);
+  return Promise.allSettled([user, photo]).then((res) => {
+    const array = [];
+    res.forEach((elem) => {
+      if (elem.status === 'fulfilled') {
+        array.push({
+          status: elem.status,
+          value: elem.value,
+        });
+      } else {
+        array.push({
+          status: elem.status,
+          value: `${elem.reason}`,
+        });
+      }
+    });
+    return array;
+  });
 }
