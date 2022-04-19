6c1a04d64dc1dbbcd409b8cceb547a4357a53b4a
root <3338@holbertonschool.com>
2022-04-19 12:43:20 +0000

    D

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,4 @@
-/*eslint-disable no-param-reassign*/
+/* eslint-disable no-param-reassign */
 export default function updateStudentGradeByCity(arrStudents, strCity, arrNewGrades) {
   return arrStudents.filter((x) => x.location === strCity).map((z) => {
     const found = arrNewGrades.find((obj) => obj.studentId === z.id);
